ecf2dc57f26236a135266c3a4ca4115cb9dceec4
Vlad Dobromyslov <vdobrik@peerplays.com>
2023-09-28 17:34:37 +0300

    Add repo

---
+++ autogen.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+###############################################################################
+#  Copyright (c) 2014-2020 libbitcoin-explorer developers (see COPYING).
+#
+#         GENERATED SOURCE CODE, DO NOT EDIT EXCEPT EXPERIMENTALLY
+#
+###############################################################################
+
+autoreconf -i
